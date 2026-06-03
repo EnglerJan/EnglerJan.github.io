deff724cad74faf9920a2b88970c5dab73ed66dc
EnglerJan <hockye@seznam.cz>
2026-06-03 03:00:15 +0200

    Automatic deployment by deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 dir="$HOME/sc/EnglerJan.github.io"
 cd "$dir" || exit 1
 
-cp "$dir/symlinks/*" "$dir/docs/"
+cp "$dir"/symlinks/* "$dir/docs/"
 git add .
 git ci "Automatic deployment by deploy.sh"
 git push
